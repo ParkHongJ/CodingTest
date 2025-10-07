6ca3fe59805138d82be38e76ab7e009b6db1a8bd
ParkHongJ <qkrghdwns98@naver.com>
2025-10-07 20:02:29 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#pragma warning(disable :4996)
+using namespace std;
+
+#define X first
+#define Y second
+
+//상하좌우
+int dx[4] = { 0, 0, -1, 1};
+int dy[4] = { -1, 1, 0, 0};
+
+int N, M;
+int arr[10];
+bool isUsed[10];
+
+void func(int k)
+{
+	if (M == k)
+	{
+		for (int i = 0; i < k; ++i)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 1; i <= N; ++i)
+	{
+		if (isUsed[i] == false)
+		{
+			arr[k] = i;
+			isUsed[i] = true;
+			func(k + 1);
+			isUsed[i] = false;
+		}
+	}
+}
+void BackJoon()
+{
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+	
+	//1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+	cin >> N >> M;
+	func(0);
+}
+
+int main()
+{
+	//Programmers();
+	BackJoon();
+	return 0;
+}
