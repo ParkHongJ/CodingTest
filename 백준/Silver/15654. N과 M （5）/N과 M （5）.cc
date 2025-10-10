b0135ccf3672581bb62644a5305e220d630ac282
ParkHongJ <qkrghdwns98@naver.com>
2025-10-10 22:09:04 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2512 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#pragma warning(disable :4996)
+using namespace std;
+
+#define X first
+#define Y second
+
+//상하좌우
+int dx[4] = { 0, 0, -1, 1};
+int dy[4] = { -1, 1, 0, 0};
+
+int N, M;
+int arr[100000];
+bool isUsed[100000];
+vector<int> v;
+//N개의 자연수와 자연수 M이 주어졌을 때, 
+// 아래 조건을 만족하는 길이가 M인 수열
+// N개의 자연수는 모두 다른 수이다.
+void func(int k)
+{
+	if (M == k)
+	{
+		for (int i = 0; i < k; ++i)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		if (isUsed[i] == false)
+		{
+			arr[k] = v[i];
+			isUsed[i] = true;
+			func(k + 1);
+			isUsed[i] = false;
+		}
+	}
+}
+void BackJoon()
+{
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+	
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		int input;
+		cin >> input;
+		v.push_back(input);
+	}
+	
+	sort(v.begin(), v.end());
+
+	func(0);
+}
+
+int main()
+{
+	//Programmers();
+	BackJoon();
+	return 0;
+}
