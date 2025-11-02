aa73debf62e4a71ac13d53a7840747f4971d6710
ParkHongJ <qkrghdwns98@naver.com>
2025-11-02 14:46:50 +0900

    [Gold V] Title: 다이어트, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1484. 다이어트/다이어트.cc
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#pragma warning(disable :4996)
+using namespace std;
+
+//우좌상하
+int dx[4] = { 0, 1, 0, -1};
+int dy[4] = { -1, 0, 1, 0};
+
+bool visit[15] = { false };
+
+int answer = 0;
+vector<long long> v;
+
+int N;
+long long L, R, X;
+long long best = 0;
+long long worst = 0;
+long long sum = 0;
+int cnt = 0;
+//현재 고른 난이도, 난이도의 합, 최고난이도, 최저난이도, 몇개골랐는지
+void func(int st)
+{
+	if (cnt >= 2 && best - worst >= X)
+	{
+		if (sum >= L && sum <= R)
+		{
+			++answer;
+		}
+	}
+
+	for (int i = st; i <= N; ++i)
+	{
+		if (visit[i] == true)
+			continue;
+		if (sum + v[i] > R)
+			continue;
+
+		visit[i] = true;
+		sum += v[i];
+		long long prevBest = best;
+		long long prevWorst = worst;
+		if (cnt == 0)
+		{
+			best = worst = v[i];
+		}
+		else
+		{
+			best = max(best, v[i]);
+			worst = min(worst, v[i]);
+		}
+		++cnt;
+		func(i);
+		--cnt;
+		sum -= v[i];
+		visit[i] = false;
+		best = prevBest;
+		worst = prevWorst;
+
+	}
+}
+
+void BackJoon()
+{
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+	
+	// G
+	long long G;
+	cin >> G;
+
+	long long cur = 2, before = 1;
+
+	vector<long long> answer;
+	while (true)
+	{
+		if ((cur * cur) - (before * before) == G)
+		{
+			answer.push_back(cur);
+			++cur;
+		}
+		else if ((cur * cur) - (before * before) < G)
+		{
+			++cur;
+		}
+		else if ((cur * cur) - (before * before) > G)
+		{
+			++before;
+		}
+
+		if (before + 1 == cur && (cur * cur - before * before) > G) 
+			break;
+		//if (2 * cur - 1 > G && before + 1 == cur) 
+		//	break;
+	}
+
+	if (answer.empty())
+	{
+		cout << -1 << '\n';
+		return;
+	}
+
+	sort(answer.begin(), answer.end());
+
+	for (int i = 0; i < answer.size(); i++)
+	{
+		cout << answer[i] << '\n';
+	}
+}
+
+int main()
+{
+	//Programmers();
+	BackJoon();
+	return 0;
+}
