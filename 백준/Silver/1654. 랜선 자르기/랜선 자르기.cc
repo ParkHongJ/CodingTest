549c44043dd665614e55e334886a85e33b3a2e33
ParkHongJ <qkrghdwns98@naver.com>
2025-12-29 12:09:02 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <list>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <unordered_set>
+#include <cmath>
+#pragma warning(disable :4996)
+using namespace std;
+
+void BackJoon()
+{
+	cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+
+	//갖고있는 랜선 K
+	//필요한 랜선 N
+
+	int K, N;
+	cin >> K >> N;
+
+	vector<long> ran(K);
+	long tempsum = 0;
+	for (int i = 0; i < K; i++)
+	{
+		cin >> ran[i];
+		tempsum += ran[i];
+	}
+
+	sort(ran.begin(), ran.end());
+
+	long high = tempsum / N;
+	long low = 0;
+	long length = 0;
+
+	while (high >= low)
+	{
+		long cut = (high + low) / 2;
+		int num = 0;
+        if (cut == 0)
+        {
+            cout << 1 <<'\n';
+            return;
+        }
+		
+		for (int i = 0; i < ran.size(); ++i)
+		{
+            if (cut > 0)
+			    num += ran[i] / cut;
+		}
+
+		//필요한 개수를 넘었다
+		//길이를 늘림
+		if (num >= N)
+		{
+			length = cut;
+			low = cut + 1;
+		}
+		else
+		{
+			//개수가 부족하다. 
+			//길이를 줄임
+			high = cut - 1;
+		}
+	}
+
+	cout << length << '\n';
+}
+
+int main()
+{
+	//Programmers();
+	BackJoon();
+	return 0;
+}
